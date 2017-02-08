09278a4fc3a132b9e9ddd632eb8938ca85741fb1
Unknown <techinsiderpro@gmail.com>
2017-02-08 18:59:24 -0500

    Created basic ItemStack class

---
+++ src/trade/ItemStack.java
@@ -0,0 +1,62 @@
+package trade;
+
+/**
+ * Created by Alex on 2/8/2017.
+ */
+public class ItemStack
+{
+    private static final int STACK_MAX = 64;
+    private Item stackItem;
+    private int count;
+
+    public ItemStack(Item stackItem)
+    {
+        this.stackItem = stackItem;
+        this.count = 1;
+    }
+
+    public ItemStack(Item stackItem, int count)
+    {
+        this.stackItem = stackItem;
+        this.count = count;
+    }
+
+    public int add(Item item, int itemCount)
+    {
+        if (item.equals(stackItem) && itemCount > -1)
+        {
+            int amountToAdd = (itemCount <= STACK_MAX - count) ? itemCount : STACK_MAX - count;
+            count += amountToAdd;
+            return amountToAdd;
+        }
+
+        return -1;
+    }
+
+    public void addFromStack(ItemStack itemStack, int itemCount)
+    {
+        itemStack.remove(this.add(itemStack.getStackItem(), Math.min(itemCount, itemStack.getCount())));
+    }
+
+    public int remove(int itemCount)
+    {
+        if (itemCount > -1)
+        {
+            int amountToRemove = (count - itemCount >= 0) ? itemCount : count;
+            count -= amountToRemove;
+            return amountToRemove;
+        }
+
+        return -1;
+    }
+
+    public Item getStackItem()
+    {
+        return stackItem;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+}
